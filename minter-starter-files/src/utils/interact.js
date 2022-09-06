d9e2bcef19f4cb61cdda6fe7b5cb1e1a3dd8a236
ESwan072 <ethan1505@gmail.com>
2022-09-06 18:36:36 +0200

    i'm in hell

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -135,7 +135,8 @@ export const connectWallet = async () => {
       //needs to match the active address of the user
       from: window.ethereum.selectedAddress, 
       //make a call to the smart contract
-      'data': window.contract.methods.mintNFT('0x1d66cE0EC4966A3E057F626b176C4Eb82D4Fb09A', TokenURI).encodeABI() 
+      //can send to specific addresses
+      'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, TokenURI).encodeABI() 
     };
 
     //sign transaction via metamask
